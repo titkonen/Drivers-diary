dfb74657b8a86651cd43ab56f90bdfe4bb6a1c15
Toni Itkonen <toni.itkonen@icloud.com>
2021-12-17 13:12:03 +0200

    Initial project commit

---
+++ My-Driving-Diary/My-Driving-Diary/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-Driving-Diary
+//
+//  Created by Toni Itkonen on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
